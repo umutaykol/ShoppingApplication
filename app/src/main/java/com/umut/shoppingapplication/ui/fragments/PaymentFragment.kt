127ee68842363bd03b76b97cd069dade9dc6221f
Mehmet Aykol Aykol <68656459+umutaykol@users.noreply.github.com>
2023-03-31 18:47:50 +0300

    Payment And Payment Result Fragment Added

---
+++ app/src/main/java/com/umut/shoppingapplication/ui/fragments/PaymentFragment.kt
@@ -0,0 +1,31 @@
+package com.umut.shoppingapplication.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.umut.shoppingapplication.R
+import com.umut.shoppingapplication.databinding.FragmentPaymentBinding
+import com.umut.shoppingapplication.databinding.FragmentPaymentResultBinding
+import com.umut.shoppingapplication.ui.viewmodels.PaymentFragmentViewModel
+import com.umut.shoppingapplication.ui.viewmodels.PaymentResultViewModel
+
+class PaymentFragment : Fragment() {
+
+    lateinit var binding: FragmentPaymentBinding
+
+    private val paymentFragmentViewModel: PaymentFragmentViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPaymentBinding.inflate(inflater, container, false)
+
+        
+        return binding.root
+    }
+
+}
