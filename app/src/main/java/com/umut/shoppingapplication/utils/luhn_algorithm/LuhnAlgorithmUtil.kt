fbcc8f6261febdc5b4b4eb7da8dc1306bd93acef
Mehmet Aykol Aykol <68656459+umutaykol@users.noreply.github.com>
2023-04-03 01:34:21 +0300

    Code Review

--- app/src/main/java/com/umut/shoppingapplication/utils/luhn_algorithm/LuhnAlgorithmUtil.kt
+++ app/src/main/java/com/umut/shoppingapplication/utils/luhn_algorithm/LuhnAlgorithmUtil.kt
@@ -1,5 +1,6 @@
 package com.umut.shoppingapplication.utils.luhn_algorithm
 
+import com.umut.shoppingapplication.utils.Constants.character_zero
 import com.umut.shoppingapplication.utils.Constants.white_space
 
 fun String.checkLuhnAlgorithm(): Boolean {
@@ -10,11 +11,11 @@ fun String.checkLuhnAlgorithm(): Boolean {
     var checksum = 0
 
     for (i in creditCardNumber.length - 1 downTo 0 step 2) {
-        checksum += creditCardNumber[i] - '0'
+        checksum += creditCardNumber[i] - character_zero
     }
 
     for (i in creditCardNumber.length - 2 downTo 0 step 2) {
-        val n: Int = (creditCardNumber[i] - '0') * 2
+        val n: Int = (creditCardNumber[i] - character_zero) * 2
         checksum += if (n > 9) n - 9 else n
     }
 
