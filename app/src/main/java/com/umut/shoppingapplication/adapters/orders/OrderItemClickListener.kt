a7564a0ebb3adceb921e5b2653a7f0cae032611e
Mehmet Aykol Aykol <68656459+umutaykol@users.noreply.github.com>
2023-04-02 19:02:54 +0300

    OrderRecyclerView With order_item layout reorganized

--- app/src/main/java/com/umut/shoppingapplication/adapters/orders/OrderItemClickListener.kt
+++ app/src/main/java/com/umut/shoppingapplication/adapters/orders/OrderItemClickListener.kt
@@ -1,12 +1,10 @@
 package com.umut.shoppingapplication.adapters.orders
 
 import android.view.View
-import com.umut.shoppingapplication.models.Product
+import com.umut.shoppingapplication.models.Order
 
 interface OrderItemClickListener {
 
-    fun deleteOrderClicked(view: View, product: Product, position: Int)
-
-    fun orderInformationClicked(view: View, product: Product, position: Int)
+    fun deleteOrderClicked(view: View, product: Order, position: Int)
 
 }
--- app/src/main/java/com/umut/shoppingapplication/ui/payment/orders_screen/OrdersFragment.kt
+++ app/src/main/java/com/umut/shoppingapplication/ui/payment/orders_screen/OrdersFragment.kt
@@ -4,21 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.umut.shoppingapplication.adapters.orders.OrderItemClickListener
 import com.umut.shoppingapplication.databinding.FragmentOrdersBinding
+import com.umut.shoppingapplication.models.Order
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class OrdersFragment : Fragment() {
+class OrdersFragment : Fragment(), OrderItemClickListener {
 
     private val ordersFragmentViewModel: OrdersFragmentViewModel by viewModels()
 
     lateinit var binding: FragmentOrdersBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentOrdersBinding.inflate(inflater, container, false)
 
@@ -28,7 +30,7 @@ class OrdersFragment : Fragment() {
 
         ordersFragmentViewModel.getAllOrdersFromRepository()
 
-        TODO( "Orderlar silinebilecek.")
+        TODO("Orderlar silinebilecek.")
 
         return binding.root
     }
@@ -50,4 +52,34 @@ class OrdersFragment : Fragment() {
             }
         }
     }
+
+    override fun deleteOrderClicked(view: View, order: Order, position: Int) {
+        showAlertDialog(
+            title = "UYARI",
+            message = "Sipariş silinecektir, emin misiniz?",
+            negativeButtonText = "Hayır",
+            positiveButtonText = "Evet",
+            negativeButtonFunction = { /* Do Nothing */ },
+            positiveButtonFunction = { TODO("Sipariş DB'den silinecek.") }
+        )
+    }
+
+    private fun showAlertDialog(
+        title: String,
+        message: String,
+        negativeButtonText: String,
+        positiveButtonText: String,
+        negativeButtonFunction: () -> Unit,
+        positiveButtonFunction: () -> Unit
+    ) {
+        AlertDialog.Builder(requireContext())
+            .setMessage(message)
+            .setTitle(title)
+            .setCancelable(false)
+            .setNegativeButton(negativeButtonText) { _, _ -> negativeButtonFunction() }
+            .setPositiveButton(positiveButtonText) { _, _ -> positiveButtonFunction() }
+            .create()
+            .show()
+    }
+
 }
